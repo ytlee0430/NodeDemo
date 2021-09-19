3a09e92f0b6fcead37dc20342e5160bea98e29dd
Bruce <bruce@emq.com>
2021-09-19 14:42:22 +0800

    fix: not found handling, do not expose detail in general API

--- index.js
+++ index.js
@@ -17,19 +17,25 @@ const users = UsersModel(sequelize, Sequelize.DataTypes)
 
 app.get('/users', auth, (req, res) => {
   users.findAll().then((allUsers) => {
+    if (allUsers.length == 0) {
+      res.status(404).send('users resource not found')
+    }
+
     const allUsersData = allUsers.map((u) => {
-      return u.dataValues
+      return u.dataValues.acct
     })
-    return res.send({ users_data: allUsersData })
+
+    return res.send({ users: allUsersData })
   })
 })
 
+// is find 'an' user by fullname, what if fullnames are duplicated in db?
 app.get('/users/:fullname/fullname', auth, (req, res) => {
-  users.findAll({ where: { fullname: req.params.fullname } }).then((allUsers) => {
-    const allUsersData = allUsers.map((u) => {
-      return u.dataValues
-    })
-    return res.send({ users_data: allUsersData })
+  users.findOne({ where: { fullname: req.params.fullname } }).then((user) => {
+    if (!user) {
+      res.status(404).send('fullname not found')
+    }
+    return res.send({ user: user.acct })
   })
 })
 
