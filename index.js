a73f88d9eae557782b3f920d656c6d07672548ba
Bruce <bruce@emq.com>
2021-09-20 23:47:48 +0800

    fix: body field validation, error handling

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(Express.json())
 app.get('/users', auth, (req, res) => {
   users.findAll().then((allUsers) => {
     if (allUsers.length == 0) {
-      res.status(404).send('users resource not found')
+      res.status(404).send({ message: 'users resource not found' })
     }
 
     const allUsersData = allUsers.map((u) => {
@@ -39,7 +39,7 @@ app.get('/users', auth, (req, res) => {
 app.get('/users/:fullname/fullname', auth, (req, res) => {
   users.findOne({ where: { fullname: req.params.fullname } }).then((user) => {
     if (!user) {
-      res.status(404).send('fullname not found')
+      res.status(404).send({ message: 'fullname not found' })
     }
     return res.send({ user: user.acct })
   })
@@ -48,7 +48,7 @@ app.get('/users/:fullname/fullname', auth, (req, res) => {
 app.get('/users/:account/account', authAccount, (req, res) => {
   users.findByPk(req.params.account).then((user) => {
     if (!user) {
-      res.status(404).send('user not found')
+      res.status(404).send({ message: 'user not found' })
     }
     return res.send({ user })
   })
@@ -67,5 +67,16 @@ app.post('/users', body('account').isLength({ max: 32 }),
       return res.status(201).send({ user, token })
     })
   })
+
+app.post('/users/authenticate', body('account').isLength({ max: 32 }), body('pwd').isLength({ max: 32 }), (req, res) => {
+  users.findOne({ where: { acct: req.body.account, pwd: req.body.pwd } }).then((user) => {
+    if (user == null) {
+      return res.status(401).send({ message: 'authenticate fail' })
+    }
+
+    const token = jwt.sign(user.toJSON(), app.get('secret'), { expiresIn: '600m' })
+    return res.status(201).send({ user, token })
+  })
+})
 
 app.listen(5000, () => { return console.log('Server up on port 5000') })
